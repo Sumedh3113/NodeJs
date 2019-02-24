8ad5164dd559d1ab4566b8535f5564a442676428
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-02-24 03:03:48 -0800

    product file

--- SwagShopApi/models/product.js
+++ SwagShopApi/models/product.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var product = new Schema({
-    title:String,
-    price:Number,
-    likes:Number
+    title: String,
+    price: Number,
+    likes:{type: Number, default: 0}
     
 });
 
-module.export = mongoose.model('Product',product);
+module.export = mongoose.model('Product', product);
