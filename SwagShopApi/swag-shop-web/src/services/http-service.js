1929a4579cd5501d6c23cedbf3d42edb2597f4f8
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-03-05 22:42:17 -0800

    http service

--- SwagShopApi/swag-shop-web/src/services/http-service.js
+++ SwagShopApi/swag-shop-web/src/services/http-service.js
@@ -2,17 +2,19 @@ import 'whatwg-fetch';
 
 
 class HttpService {
-    
+    //1
     getProducts = ()=>{
+        //2
         var promise = new Promise((resolve,reject)=>{
-        
+        //4
             fetch('http://localhost:3004/product').then(res=>{
-            //resolve(res.json());
-            reject('Fuck You');
+            resolve(res.json());
+            //reject('Error occured');
         })
     
             
         });
+        // 3
         return promise;
                 
     }
