016730bacf6ec77b0a40f49901a909e7ac3c9739
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-03-07 00:30:37 -0800

    service file

--- SwagShopApi/swag-shop-web/src/services/http-service.js
+++ SwagShopApi/swag-shop-web/src/services/http-service.js
@@ -6,7 +6,7 @@ class HttpService {
     getProducts = ()=>{
         //2
         var promise = new Promise((resolve,reject)=>{
-        //4
+        //4 take data from the server fetching from db is done at //server.js page
             fetch('http://localhost:3004/product').then(res=>{
             resolve(res.json());
             //reject('Error occured');
