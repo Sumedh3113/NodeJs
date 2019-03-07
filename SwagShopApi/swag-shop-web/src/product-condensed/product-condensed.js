ff4da64690967307701a0a90529720c3e05694fb
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-03-07 00:31:25 -0800

    product inside wishlist

--- SwagShopApi/swag-shop-web/src/product-condensed/product-condensed.js
+++ SwagShopApi/swag-shop-web/src/product-condensed/product-condensed.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import './product-condensed.css';
+import DataService from '../services/data-service';
+
+let ds = new DataService();
 
 
 class ProductCondensed extends Component{
-
+    constructor(props){
+        super(props);
+        
+        
+        this.removeProduct = this.removeProduct.bind(this);
+        
+    }
+    
+    removeProduct = () => {
+        
+        ds.removeFromWishList(this.props.product);
+    }
+    
+// this.pros.title is supplied from wishlist.js ProductCondensed //tag
     render(){
         return(
-        <li className="list group-item">
-            <a className="btn btn-outline">{this.props.title}|${this.props.price}</a>        
+        <li className="list-group-item">
+            <a className="btn btn-outline-danger" onClick = {() => this.removeProduct()}>X</a>
+                {this.props.product.title} | <b>${this.props.product.price}</b>
             
             </li>
         );
