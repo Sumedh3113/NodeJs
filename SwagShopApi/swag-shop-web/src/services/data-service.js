91c5e270da3f04e0efee4f8c610faada0bc8f1a0
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-03-07 00:30:33 -0800

    service file

---
+++ SwagShopApi/swag-shop-web/src/services/data-service.js
@@ -0,0 +1,52 @@
+import NotificationService,{NOTIF_WISHLIST_CHANGED} from './notification-service'
+
+let ns = new NotificationService();
+
+let instance =null;
+var wishList = [];
+
+
+class DataService {
+   constructor() {
+       if (!instance) {
+        instance = this;
+    }
+    
+    return instance;
+  
+       
+   } 
+    
+    itemOnWishList = item => {
+        for(var x = 0; x< wishList.length; x++){
+            if (wishList[x]._id === item._id){
+                return true;
+            }
+            
+        }
+        
+        return false;
+    }
+    
+    addToWishList = item =>{
+        wishList.push(item);
+        ns.postNotification(NOTIF_WISHLIST_CHANGED, wishList);
+        
+    }
+    
+    removeFromWishList = item => {
+        for(var x=0;x<wishList.length;x++){
+            if(wishList[x]._id === item._id){
+            wishList.splice(x,1);
+        ns.postNotification(NOTIF_WISHLIST_CHANGED, wishList);
+            break;
+            }
+            
+        }
+        
+    }
+      
+    
+}
+
+export default DataService;
